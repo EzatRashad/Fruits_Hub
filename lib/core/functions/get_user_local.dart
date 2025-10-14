a3e55aff83a2bbc63250018a5d63daf8395f2d8f
Ezat Rashad <123140512+EzatRashad@users.noreply.github.com>
2025-10-14 22:22:32 +0300

    fix: improve user retrieval logic to handle empty local storage

--- lib/core/functions/get_user_local.dart
+++ lib/core/functions/get_user_local.dart
@@ -4,9 +4,18 @@ import 'package:fruit_hub/core/services/shared_pref_service/shared_preferences_s
 import 'package:fruit_hub/core/utils/constants.dart';
 import 'package:fruit_hub/features/auth/data/models/user_model.dart';
 import 'package:fruit_hub/features/auth/domain/entities/user_entity.dart';
-
 UserEntity getUserLocal() {
   var jsonString = SharedPreferencesService.getString(userData);
-  var userEntity = UserModel.fromJson(jsonDecode(jsonString ?? ""));
-  return userEntity;
+
+  if (jsonString == null || jsonString.isEmpty) {
+    return UserModel(
+       
+      name: "ضيف",
+      email: "",
+       uId: '',
+       
+    );
+  }
+
+  return UserModel.fromJson(jsonDecode(jsonString));
 }
